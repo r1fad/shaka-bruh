be940377e6c4190b0a4a9819fbe704b85639b74a
Rifad Lafir <rifadahmedlafir@gmail.com>
2017-10-28 22:49:38 +0100

    remember url

--- src/leap_shaka.py
+++ src/leap_shaka.py
@@ -2,17 +2,40 @@ import os, sys, inspect
 src_dir = os.path.dirname(inspect.getfile(inspect.currentframe()))
 lib_dir = os.path.abspath(os.path.join(src_dir, '../lib'))
 sys.path.insert(0, lib_dir)
+
 import Leap
 import time
+from PIL import Image
+import urllib, cStringIO
+from random import randint
+import os
+import psutil
+os.system("sudo service leapd restart")
+
+f = open("memez.txt","r")
+memez = f.readlines()
+
 
 class SampleListener(Leap.Listener):
     frame_count = 0
+    currentUrl = ''
+    sent = False
     def on_connect(self, controller):
         print "Connected"
 
-    def on_frame(self, controller):
+        #enable gestures
+        controller.enable_gesture(Leap.Gesture.TYPE_SWIPE)
 
+    def on_frame(self, controller):
         frame = controller.frame()
+
+        for gesture in frame.gestures():
+            if gesture.type is Leap.Gesture.TYPE_SWIPE:
+                swipe = Leap.SwipeGesture(gesture)
+                if not SampleListener.sent:
+                    print SampleListener.currentUrl
+                    SampleListener.sent = True
+
         shaka_thumb = False
         shaka_pinky = False
         not_fingers = False
@@ -33,8 +56,24 @@ class SampleListener(Leap.Listener):
 
         if SampleListener.frame_count == 60:
             print 'get a meme'
+            SampleListener.sent = False
+
+            for proc in psutil.process_iter():
+                if proc.name() == "display":
+                    proc.kill()
+
+            URL = memez[randint(0,len(memez))]
+            SampleListener.currentUrl = URL
+            file1 = cStringIO.StringIO(urllib.urlopen(URL).read())
+            img = Image.open(file1)
+            basewidth = 1500
+            wpercent = (basewidth/float(img.size[0]))
+            hsize = int((float(img.size[1])*float(wpercent)))
+            img = img.resize((basewidth,hsize), Image.ANTIALIAS)
+            img.show()
             time.sleep(0.5)
             SampleListener.frame_count = 0
+
 
 
 def main():
