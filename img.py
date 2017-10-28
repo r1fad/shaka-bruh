c2c762bba19bc951ea3ddebde75e2090d7d58a62
Mihnea Savu <mihnea.savu@student.manchester.ac.uk.com>
2017-10-28 16:46:37 +0100

    add random meme displaying

---
+++ img.py
@@ -0,0 +1,20 @@
+
+from PIL import Image
+import urllib, cStringIO
+from random import randint
+
+f = open("memez.txt","r")
+memez = f.readlines()
+
+
+
+
+URL = memez[randint(0,len(memez))]
+file = cStringIO.StringIO(urllib.urlopen(URL).read())
+img = Image.open(file)
+basewidth = 600
+wpercent = (basewidth/float(img.size[0]))
+hsize = int((float(img.size[1])*float(wpercent)))
+img = img.resize((basewidth,hsize), Image.ANTIALIAS)
+
+img.show()
